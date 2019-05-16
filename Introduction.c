64aeeb6e2bfdaaaf38c6b84393b4db8bf1061588
Periyar Payilagam <49941426+periyarpayilagam@users.noreply.github.com>
2019-05-16 22:40:26 +0530

    Create Introduction.c

---
+++ Introduction.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+   printf("Welcome!");
+   return 0;
+}
+/*
+in cmd,
+    > gcc Intro.c
+	> a.exe
+	
+	or
+    > gcc -o Operators Operators.c
+    > Operators
+	
+*/
